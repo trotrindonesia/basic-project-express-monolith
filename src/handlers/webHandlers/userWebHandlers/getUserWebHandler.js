409cc537cad9f1cde7420413990df58755929e11
Cristoph Adam Sugianto <giezbungadeza@gmail.com>
2020-07-25 00:27:44 +0700

    implement nunjucks view engine

--- src/handlers/webHandlers/userWebHandlers/getUserWebHandler.js
+++ src/handlers/webHandlers/userWebHandlers/getUserWebHandler.js
@@ -2,9 +2,10 @@ const getUserHandler = async (req, res) => {
   const { UserDbConnector } = res.locals;
   const users = await UserDbConnector.find();
   const data = {
+    title: 'User',
     users
   };
-  res.render('web/users/index', data);
+  res.render('contents/users/userView.html', data);
 };
 
 module.exports = getUserHandler;
--- src/preloaders/templateLoader.js
+++ src/preloaders/templateLoader.js
@@ -1,17 +1,13 @@
-const hbs = require('express-handlebars');
+const nunjucks = require('nunjucks');
 const path = require('path');
 
 const viewPath = path.join(__dirname, '../views');
 
 const templateLoader = (app) => {
-  app.engine('hbs', hbs({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: path.join(viewPath, 'layouts/main'),
-    partialsDir: path.join(viewPath, 'layouts/partials')
-  }));
-  app.set('view engine', '.hbs');
-  app.set('views', viewPath);
+  nunjucks.configure(viewPath, {
+    autoescape: true,
+    express: app
+  });
 };
 
 module.exports = templateLoader;
