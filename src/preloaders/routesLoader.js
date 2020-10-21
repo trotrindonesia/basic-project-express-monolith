6dcbb6690d8463d3f1252a8a33bc953dd65a5e9b
Cristoph Adam Sugianto <giezbungadeza@gmail.com>
2020-10-21 19:50:26 +0700

    add express.json() and express.urlencoded({ extended: true }) to route loader

--- src/preloaders/routesLoader.js
+++ src/preloaders/routesLoader.js
@@ -1,6 +1,10 @@
+const express = require('express');
+
 const { webRouters, apiRouters } = require('../routes');
 
 const routesLoader = (app) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
   app.use('/api', apiRouters);
   app.use(webRouters);
 };
